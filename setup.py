cc850e48fe49ff8e539d65770a673525c73eef97
Stef <sstefin@bk.ru>
2020-07-26 15:19:04 -0500

    :arrow_up: Bumping version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ URL = 'https://github.com/IamStefin/wdcrypt'
 EMAIL = 'sstefin@bk.ru'
 AUTHOR = 'Stef'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '1.3'
+VERSION = '1.3.1'
 
 REQUIRED = [
     'cryptography','colorama'
